987414b853719a886a60fb9d3e4c9b54b5e9b366
Gyandeep Aryan <gyandeeparyan2@gmail.com>
2023-04-20 12:02:13 +0530

    images updated

--- src/components/leftBar/LeftBar.jsx
+++ src/components/leftBar/LeftBar.jsx
@@ -1,93 +1,77 @@
 import "./leftBar.scss";
-import Friends from "../../assets/1.png";
-import Groups from "../../assets/2.png";
-import Market from "../../assets/3.png";
-import Watch from "../../assets/4.png";
-import Memories from "../../assets/5.png";
-import Events from "../../assets/6.png";
-import Gaming from "../../assets/7.png";
-import Gallery from "../../assets/8.png";
-import Videos from "../../assets/9.png";
-import Messages from "../../assets/10.png";
-import Tutorials from "../../assets/11.png";
-import Courses from "../../assets/12.png";
-import Fund from "../../assets/13.png";
+
 import { AuthContext } from "../../context/authContext";
 import { useContext } from "react";
 
 const LeftBar = () => {
-
   const { currentUser } = useContext(AuthContext);
 
   return (
-    <div className="leftBar">
-      <div className="container">
-        <div className="menu">
-          <div className="user">
-            <img
-              src={currentUser.profilePic}
-              alt=""
-            />
+    <div className='leftBar'>
+      <div className='container'>
+        <div className='menu'>
+          <div className='user'>
+            <img src={currentUser.profilePic} alt='' />
             <span>{currentUser.name}</span>
           </div>
-          <div className="item">
-            <img src={Friends} alt="" />
+          <div className='item'>
+            <img src='https://i.ibb.co/wJwfL6P/friend.png' alt='' />
             <span>Friends</span>
           </div>
-          <div className="item">
-            <img src={Groups} alt="" />
+          <div className='item'>
+            <img src='https://i.ibb.co/nLv4kNv/2.png' alt='' />
             <span>Groups</span>
           </div>
-          <div className="item">
-            <img src={Market} alt="" />
+          <div className='item'>
+            <img src='https://i.ibb.co/WcjnR95/3.png' alt='' />
             <span>Marketplace</span>
           </div>
-          <div className="item">
-            <img src={Watch} alt="" />
+          <div className='item'>
+            <img src='https://i.ibb.co/Y2fFFJ1/4.png' alt='' />
             <span>Watch</span>
           </div>
-          <div className="item">
-            <img src={Memories} alt="" />
+          <div className='item'>
+            <img src='https://i.ibb.co/RHcYqWV/5.png' alt='' />
             <span>Memories</span>
           </div>
         </div>
         <hr />
-        <div className="menu">
+        <div className='menu'>
           <span>Your shortcuts</span>
-          <div className="item">
-            <img src={Events} alt="" />
+          <div className='item'>
+            <img src='https://i.ibb.co/DfpkHCq/6.png' alt='' />
             <span>Events</span>
           </div>
-          <div className="item">
-            <img src={Gaming} alt="" />
+          <div className='item'>
+            <img src='https://i.ibb.co/LhXhLy5/7.png' alt='' />
             <span>Gaming</span>
           </div>
-          <div className="item">
-            <img src={Gallery} alt="" />
+          <div className='item'>
+            <img src='https://i.ibb.co/vvKfJQx/8.png' alt='' />
             <span>Gallery</span>
           </div>
-          <div className="item">
-            <img src={Videos} alt="" />
+          <div className='item'>
+            <img src='https://i.ibb.co/3177Mcz/9.png' alt='' />
             <span>Videos</span>
           </div>
-          <div className="item">
-            <img src={Messages} alt="" />
+          <div className='item'>
+            <img src='https://i.ibb.co/YyPdbYZ/10.png' alt='' />
             <span>Messages</span>
           </div>
         </div>
         <hr />
-        <div className="menu">
+        <div className='menu'>
           <span>Others</span>
-          <div className="item">
-            <img src={Fund} alt="" />
+          <div className='item'>
+            <img src='https://i.ibb.co/DzJjmZd/13.png' alt='' />
             <span>Fundraiser</span>
           </div>
-          <div className="item">
-            <img src={Tutorials} alt="" />
+          <div className='item'>
+            <img src='https://i.ibb.co/tPwWBFt/11.png' alt='' />
             <span>Tutorials</span>
           </div>
-          <div className="item">
-            <img src={Courses} alt="" />
+          <div className='item'>
+            <img src='https://i.ibb.co/BByrwqH/12.png' alt='' />
             <span>Courses</span>
           </div>
         </div>
--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -1,42 +1,40 @@
 import "./share.scss";
-import Image from "../../assets/img.png";
-import Map from "../../assets/map.png";
-import Friend from "../../assets/friend.png";
+
 import { useContext } from "react";
 import { AuthContext } from "../../context/authContext";
 
 const Share = () => {
   const { currentUser } = useContext(AuthContext);
   return (
-    <div className="share">
-      <div className="container">
-        <div className="top">
-          <img src={currentUser.profilePic} alt="" />
+    <div className='share'>
+      <div className='container'>
+        <div className='top'>
+          <img src={currentUser.profilePic} alt='' />
           <input
-            type="text"
+            type='text'
             placeholder={`What's on your mind ${currentUser.name}?`}
           />
         </div>
         <hr />
-        <div className="bottom">
-          <div className="left">
-            <input type="file" id="file" style={{ display: "none" }} />
-            <label htmlFor="file">
-              <div className="item">
-                <img src={Image} alt="" />
+        <div className='bottom'>
+          <div className='left'>
+            <input type='file' id='file' style={{ display: "none" }} />
+            <label htmlFor='file'>
+              <div className='item'>
+                <img src='https://i.ibb.co/jRVVhsp/img.png' alt='' />
                 <span>Add Image</span>
               </div>
             </label>
-            <div className="item">
-              <img src={Map} alt="" />
+            <div className='item'>
+              <img src='https://i.ibb.co/QKhKyJs/map.png' alt='' />
               <span>Add Place</span>
             </div>
-            <div className="item">
-              <img src={Friend} alt="" />
+            <div className='item'>
+              <img src='https://i.ibb.co/wJwfL6P/friend.png' alt='' />
               <span>Tag Friends</span>
             </div>
           </div>
-          <div className="right">
+          <div className='right'>
             <button>Post</button>
           </div>
         </div>
